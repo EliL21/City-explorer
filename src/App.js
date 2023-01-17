c04c17f06c0b28d883ac13bafa29b693aa6a6ddb
eli bailey <baileyeli19@gmail.com>
2023-01-17 17:59:05 +0000

    city explorer

--- src/App.js
+++ src/App.js
@@ -2,27 +2,51 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-
+      location: '',
+      locationData: []
     }
   }
 
-  handleGetLocation = () => {
-
+  handleInput = (e) => {
+    this.setState({
+      location: e.target.value
+    })
   }
-  render(){
-    return(
+  locationData = async (e) => {
+    e.preventDefault();
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.location}`;
+    try {
+      let location = await axios.get(url)
+      
+      this.setState({
+        locationData: location.data
+      })
+      // console.log(location.data);
+    } catch(error){
+
+    }
+
+   
+  }
+  render() {
+    return (
       <>
-      <form>
-        <button onClick={this.handleGetLocation}>Explore!</button>
-      </form>
-      
-      
-      
-      
+        <div>
+          <h1>CITY Explore</h1>
+
+          <form onSubmit={this.locationData}>
+            <label htmlFor="">Pick a City!</label>
+            <input type="text" onInput={this.handleInput} />
+            <button type="submit">Explore!</button>
+          </form>
+          <p></p>
+        </div>
+
+
       </>
     )
   }
