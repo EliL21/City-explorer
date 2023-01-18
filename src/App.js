6d528e53e54f8b7057aa094152431591a6beca65
eli bailey <baileyeli19@gmail.com>
2023-01-18 19:39:52 +0000

    needs more work

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: '',
-      locationData: []
+      city: '',
+      cityData: [],
+      error: false,
+      errorMessage: ''
     }
   }
 
   handleInput = (e) => {
     this.setState({
-      location: e.target.value
+      city: e.target.value
     })
   }
-  locationData = async (e) => {
+
+
+  cityData = async (e) => {
     e.preventDefault();
-    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.location}`;
+    
+
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json&limit=1`;
     try {
-      let location = await axios.get(url)
-      
+
+      let cityDataFromAxios = await axios.get(url)
+      console.log(cityDataFromAxios);
       this.setState({
-        locationData: location.data
-      })
-      // console.log(location.data);
-    } catch(error){
+        cityData: cityDataFromAxios.data[0],
+
+        cityMap: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityDataFromAxios[0].lat},${cityDataFromAxios[0].lon}&zoom=10`
+      });
+      // console.log(city.data);
+    } catch (error) {
+
 
     }
 
-   
+
   }
   render() {
     return (
@@ -38,12 +49,19 @@ class App extends React.Component {
         <div>
           <h1>CITY Explore</h1>
 
-          <form onSubmit={this.locationData}>
+          <form onSubmit={this.cityData}>
             <label htmlFor="">Pick a City!</label>
-            <input type="text" onInput={this.handleInput} />
+            <input type="text" id="city" name="city" onInput={this.handleInput} />
             <button type="submit">Explore!</button>
           </form>
-          <p></p>
+          {/* <Card>
+
+
+
+
+
+          </Card> */}
+          {/* <p>{this.state.locationData[0].display_name}</p> */}
         </div>
 
 
