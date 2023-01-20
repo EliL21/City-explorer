c91824666177b320078d07809d2183b008dc095c
eli bailey <baileyeli19@gmail.com>
2023-01-20 05:21:33 +0000

    updated still needs work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import './App.css';
+import Weather from './Weather'
+import React from 'react';
 import axios from 'axios';
 // import Card from 'react-bootstrap/Card';
 
@@ -10,66 +11,97 @@ class App extends React.Component {
       city: '',
       cityData: [],
       error: false,
-      errorMessage: ''
+      errorMessage: '',
+      weatherData: []
     }
   }
 
   handleInput = (e) => {
+
     this.setState({
       city: e.target.value
-    })
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      let cityDataFromAxios = await axios.get(url)
+
+      let lat = cityDataFromAxios.data[0].lat;
+      let lon = cityDataFromAxios.data[0].lon;
+
+      this.handleGetWeather(lat, lon);
+
+      this.setState({
+        cityData: cityDataFromAxios.data[0],
+        error: false
+      });
+
+    } catch (error) {
+      console.log(error.message);
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
+  }
+
+  // TODO: DEFINE A WEATHER HANDLER TO RETREIVE DATA FROM BACKEND
+  handleGetWeather = async (lat, lon) => {
+    try {
+      // TODO: build URL
+      let url = `${process.env.REACT_APP_SERVER}/weather?lat=${lat}&lon=${lon}&searchQuery=${this.state.city}`
+
+      // TODO: Use axios to hit my server
+      let weatherDataFromAxios = await axios.get(url);
+
+      console.log(weatherDataFromAxios.data);
+      // TODO: Save that weather data to state
+        this.setState({
+          weatherData: weatherDataFromAxios.data
+        });
+
+    } catch (error) {
+      console.log(error.message);
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
   }
 
 
-  cityData = async (e) => {
-    e.preventDefault();
-    
-    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json&limit=1`;
-    try {
-      
-      let cityDataFromAxios = await axios.get(url)
-      let lat = cityDataFromAxios.data[0].lat;
-      let lon = cityDataFromAxios.data[0].lon;
-      console.log(cityDataFromAxios.data[0]);
-      console.log(lat, lon);
-      this.setState({
-        cityData: cityDataFromAxios.data[0],
-      })
-       
-        // console.log(city.data);
-        } catch (error) {
-        console.log (error.errorMessage);
-
-        }
-
-
-      }
   render() {
-        return(
+    return (
       <>
-      <div>
-        <h1>CITY Explore</h1>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label > Pick a City!
+            <input type="text" onInput={this.handleInput} />
+            <button type='submit'>Explore</button>
+          </label>
 
-        <form onSubmit={this.cityData}>
-          <label htmlFor="">Pick a City!</label>
-          <input type="text" id="city" name="city" onInput={this.handleInput} />
-          <button type="submit">Explore!</button>
         </form>
-        {/* <Card>
 
 
-
-
-
-          </Card> */}
-        <p>{this.state.cityData.display_name}</p>
-        <p>{this.state.cityData.lat}</p>
-        <p>{this.state.cityData.lon}</p>
-      </div>
-
-
+        {/* Ternary - W ? T : F */}
+        {
+          this.state.error
+            ? <p>{this.state.errorMessage}</p>
+            : <>
+              <p>{this.state.cityData.display_name}</p>
+              <p> Latitude: {this.state.cityData.lat}</p>
+              <p>Longitude: {this.state.cityData.lon}</p>
+              
+              <Weather weatherData={this.state.weatherData}/>
+            </>
+        }
       </>
     )
-    }
+  }
 }
+
 export default App;
